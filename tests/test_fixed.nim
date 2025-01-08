7c676e2b83fd5cd2520d4667e2e6373d99814fde
nitely <ecastroborsani@gmail.com>
2025-01-08 15:59:22 -0300

    wip

--- tests/test_fixed.nim
+++ tests/test_fixed.nim
@@ -3,20 +3,12 @@ import unittest2
 import ../protobuf_serialization
 
 type
-  Float2Object {.proto2.} = object
-    a {.fieldNumber: 1.}: PBOption[1'f64]
-
   Float3Object {.proto3.} = object
     a {.fieldNumber: 1.}: float32
 
 suite "Test Fixed Encoding/Decoding":
   test "Can encode/decode floats wrapped in an object":
     check:
-      Protobuf.decode(
-        Protobuf.encode(Float2Object(a: pbSome(PBOption[1'f64], 2.39'f64))),
-        Float2Object
-      ).a.get() == 2.39'f64
-
       Protobuf.decode(
         Protobuf.encode(Float3Object(a: 5.64'f32)),
         Float3Object
